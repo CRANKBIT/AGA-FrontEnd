7c15817cb972d4c03d8b27cc76876f054b895c4a
Alan <prossliu@gmail.com>
2023-06-07 17:41:02 +0930

    fix

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,5 @@
 // External imports
-import { HashRouter, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 // Local imports
 import HomePage from "../pages/Home";
@@ -8,11 +8,11 @@ import HomePage2 from "../pages/Home2";
 // Component definition
 function App() {
   return (
-    <HashRouter>
+    <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="home" element={<HomePage />} />
       <Route path="home2" element={<HomePage2 />} />
-    </HashRouter>
+    </Routes>
   );
 }
 
