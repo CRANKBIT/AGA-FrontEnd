514642dc8da487fbbc95ab7ccfbaea026be84f6c
ArielWangX <arielxiewang@gmail.com>
2023-06-09 11:14:16 +1000

    Ariel/Create-Account-Page(1c): create components

---
+++ src/components/AccountTextField/AccountTextField.tsx
@@ -0,0 +1,28 @@
+// AccountForm.tsx
+
+type InputProps = {
+    label: string
+    htmlFor: string
+    type: string
+    id: string
+    value: string
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+    placeholder: string
+}
+
+export default function AccountTextField({
+    label,
+    htmlFor,
+    type,
+    id,
+    value,
+    onChange,
+    placeholder,
+}: InputProps) {
+    return (
+        <div className="mb-[25px] w-full">
+            <label htmlFor={htmlFor} className="block text-[#1E1E1E] leading-[1.1875rem] font-medium text-base mb-[2px] font-inter">{label}</label>
+            <input type={type} id={id} value={value} onChange={onChange} placeholder={placeholder} required className="w-full bg-white text-base leading-[1.1875rem] border border-[#03111B] rounded-[5px] px-[15px] py-[10px] placeholder-[#1E1E1E] opacity-40" />
+        </div>
+    )
+}
