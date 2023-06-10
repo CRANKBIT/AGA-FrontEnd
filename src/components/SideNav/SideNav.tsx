bcf774722a1d8a5e4cd4c4683f6b54372f5ce1e5
smarthulala <chao.long.au@gmail.com>
2023-06-10 19:33:45 +1000

    Removed all saces in classnames;

--- src/components/SideNav/SideNav.tsx
+++ src/components/SideNav/SideNav.tsx
@@ -3,13 +3,13 @@ import SideNavList from './components/SideNavList'
 
 export default function SideNav() {
   return (
-    <div className="bg-[#03111b] w-[305px] text-[#FFFFFF] h-screen flex justify-center">
-      <div className="pt-[55px] ">
+    <div className="bg-background w-[305px] text-white h-screen flex justify-center">
+      <div className="pt-[55px]">
         <div className="pb-8 text-3xl">
-          <BiFile className="inline-block  text-blue-600 mr-2"></BiFile>
+          <BiFile className="inline-block text-blue-600 mr-2"></BiFile>
           <span className="text-2xl">Report Builder</span>
         </div>
-        <div className="pb-8 leading-10 ">
+        <div className="pb-8 leading-10">
           <SideNavList />
         </div>
         <div className="leading-9">
--- src/pages/KnowledgeBase/components/Content/Content.tsx
+++ src/pages/KnowledgeBase/components/Content/Content.tsx
@@ -5,13 +5,13 @@ export default function Content() {
   return (
     <div className="bg-[#E9EFF4] w-full max-w-[975px] flex justify-center pt-[55px]">
       <div>
-        <div className="text-2xl font-bold  mb-7">Knowledge Base</div>
+        <div className="text-2xl font-bold mb-7">Knowledge Base</div>
         <div className="w-[340px] text-center text-xl mx-auto">
           Explore how our Form Report can work for you
         </div>
         <div className="flex border border-black mt-5 mb-10 items-center gap-2 bg-white rounded pr-2 pl-2">
           <CiSearch className="bg-white " />
-          <input placeholder="Search…" className=" w-full focus:outline-none" />
+          <input placeholder="Search…" className="w-full focus:outline-none" />
         </div>
         <ReusableList />
       </div>
