2af3546d985f3bb20c692085464f3ba26ce207d9
jeremyhuang035 <119722002+jeremyhuang035@users.noreply.github.com>
2023-06-10 18:51:19 +1000

    fix: remove unneccessary async in services

--- src/services/user.ts
+++ src/services/user.ts
@@ -2,8 +2,8 @@ import { AxiosResponse } from 'axios'
 import api from '@/utils/axios'
 import { RegisterPayload, LoginPayload } from '@/interfaces/auth'
 
-export const registerApi = async (registerPayload: RegisterPayload): Promise<AxiosResponse> =>
+export const registerApi = (registerPayload: RegisterPayload): Promise<AxiosResponse> =>
   api.post('/auth/register', registerPayload)
 
-export const loginApi = async (loginPayload: LoginPayload): Promise<AxiosResponse> =>
+export const loginApi = (loginPayload: LoginPayload): Promise<AxiosResponse> =>
   api.post('/auth/login', loginPayload)
