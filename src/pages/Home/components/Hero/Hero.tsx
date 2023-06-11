cda9b1a9e343630afbf3183482c52b93bc4df980
Alan <prossliu@gmail.com>
2023-06-12 00:59:46 +0930

    fix: url

--- src/pages/Home/components/Hero/Hero.tsx
+++ src/pages/Home/components/Hero/Hero.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Hero: FC = () => (
   <div className="text-center py-40">
-    <img className="mx-auto" src="/svg/heroImage.svg" alt="HeroImage" />
+    <img className="mx-auto" src="./svg/heroImage.svg" alt="HeroImage" />
     <p className="text-4xl text-center mb-11">
       Unleash the Power of Mechanic:
       <br />
