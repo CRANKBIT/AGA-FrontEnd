a1cd5b5db710f53e1f521c212fa79670e71e2086
smarthulala <chao.long.au@gmail.com>
2023-06-12 22:39:57 +1000

    Delete duplicate routes in router

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -37,14 +37,6 @@ const router = createHashRouter([
     element: <KnowledgeBase />,
   },
   {
-    path: '/my-reports',
-    element: <MyReports />,
-  },
-  {
-    path: '/knowledge-base',
-    element: <KnowledgeBase />,
-  },
-  {
     path: '/account',
     element: (
       <PrivateRoute isLoggedIn={isLoggedIn}>
