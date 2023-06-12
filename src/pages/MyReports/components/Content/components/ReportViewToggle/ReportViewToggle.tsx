f9abba243ddf659a5157c0d3295033dab154afb5
smarthulala <chao.long.au@gmail.com>
2023-06-12 22:09:39 +1000

    added card view report list; toogle button for different views

--- src/pages/MyReports/components/Content/components/ReportViewToggle/ReportViewToggle.tsx
+++ src/pages/MyReports/components/Content/components/ReportViewToggle/ReportViewToggle.tsx
@@ -1,26 +1,52 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import { AiOutlineAppstore, AiOutlineMenu } from 'react-icons/ai'
 import ListView from './components/ListView'
+import CardView from './components/CardView'
 
-const ReportViewToggle: FC = () => (
-  <div className="mb-[20px] items-center">
-    <div className="flex my-2">
-      <div className="w-2/6">
-        <div className="text-gray-600">Reports</div>
+const ReportViewToggle: FC = () => {
+  const [viewMode, SetViewMode] = useState<'list' | 'card'>('list')
+
+  const handleCardView = (): undefined => {
+    SetViewMode('card')
+    return undefined
+  }
+
+  const handleListView = (): undefined => {
+    SetViewMode('list')
+    return undefined
+  }
+  const iconClassNames = 'p-0.5 inline-block rounded-lg  hover:text-white hover:bg-blue-500'
+
+  return (
+    <div className="mb-[20px] items-center">
+      <div className="flex items-center my-4">
+        <div className="w-2/6">
+          <div className="text-gray-600">Reports</div>
+        </div>
+        <div className="w-2/6">
+          <div className="text-gray-600">Last Updated</div>
+        </div>
+        <div className="w-1/6">
+          <div className="text-gray-600">Tasks</div>
+        </div>
+        <div className="w-1/6 cursor-pointer text-end text-3xl">
+          <AiOutlineAppstore
+            onClick={handleCardView}
+            className={`mr-1 ${iconClassNames} ${
+              viewMode === 'card' ? 'text-white bg-blue-500' : 'bg-gray-300 '
+            }`}
+          />
+          <AiOutlineMenu
+            onClick={handleListView}
+            className={`${iconClassNames} ${
+              viewMode === 'list' ? 'text-white bg-blue-500' : 'bg-gray-300 '
+            }`}
+          />
+        </div>
       </div>
-      <div className="w-2/6">
-        <div className="text-gray-600">Last Updated</div>
-      </div>
-      <div className="w-1/6">
-        <div className="text-gray-600">Tasks</div>
-      </div>
-      <div className="w-1/6 cursor-pointer text-end text-3xl">
-        <AiOutlineAppstore className="p-[2px] inline-block bg-gray-300 rounded mr-1 hover:text-white hover:bg-blue-500" />
-        <AiOutlineMenu className="p-[2px] inline-block bg-blue-500 rounded text-white border border-black" />
-      </div>
+      {viewMode === 'list' ? <ListView /> : <CardView />}
     </div>
-    <ListView />
-  </div>
-)
+  )
+}
 
 export default ReportViewToggle
