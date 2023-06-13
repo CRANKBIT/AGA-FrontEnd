57d1514a56b6c2282e187b4bfa14c31c31552c79
LingHan.S <LINGKUI0324@GMAIL.COM>
2023-06-13 16:23:06 +1000

    code quality improvment

--- src/pages/MyReports/components/Content/components/ReportViewToggle/ReportViewToggle.tsx
+++ src/pages/MyReports/components/Content/components/ReportViewToggle/ReportViewToggle.tsx
@@ -2,45 +2,33 @@ import { FC, useState } from 'react'
 import { AiOutlineAppstore, AiOutlineMenu } from 'react-icons/ai'
 import ListView from './components/ListView'
 import CardView from './components/CardView'
+import ToggleButton from '@/components/ToggleButton/ToggleButton'
 
 const ReportViewToggle: FC = () => {
   const [viewMode, SetViewMode] = useState<'list' | 'card'>('list')
 
-  const handleCardView = (): undefined => {
-    SetViewMode('card')
-    return undefined
-  }
-
-  const handleListView = (): undefined => {
-    SetViewMode('list')
-    return undefined
-  }
-  const iconClassNames = 'p-0.5 inline-block rounded-lg  hover:text-white hover:bg-blue-500'
-
   return (
-    <div className="mb-[20px] items-center">
-      <div className="flex items-center my-4">
-        <div className="w-2/6">
-          <div className="text-gray-600">{viewMode === 'list' ? 'Reports' : ''}</div>
-        </div>
-        <div className="w-2/6">
-          <div className="text-gray-600">{viewMode === 'list' ? 'Last Updated' : ''}</div>
-        </div>
-        <div className="w-1/6">
-          <div className="text-gray-600">{viewMode === 'list' ? 'tasks' : ''}</div>
-        </div>
+    <div>
+      <div className="flex items-center my-4 justify-between">
+        {viewMode === 'list' ? (
+          <div className="flex gap-20">
+            <div className="min-w-[140px]">Reports</div>
+            <div className="min-w-[175px]">Last Updated</div>
+            <div className="min-w-[140px]">task</div>
+          </div>
+        ) : (
+          <div />
+        )}
         <div className="w-1/6 cursor-pointer text-end text-3xl">
-          <AiOutlineAppstore
-            onClick={handleCardView}
-            className={`mr-1 ${iconClassNames} ${
-              viewMode === 'card' ? 'text-white bg-blue-500' : 'bg-gray-300 '
-            }`}
+          <ToggleButton
+            icon={AiOutlineAppstore}
+            isActive={viewMode === 'card'}
+            onClick={() => SetViewMode('card')}
           />
-          <AiOutlineMenu
-            onClick={handleListView}
-            className={`${iconClassNames} ${
-              viewMode === 'list' ? 'text-white bg-blue-500' : 'bg-gray-300 '
-            }`}
+          <ToggleButton
+            icon={AiOutlineMenu}
+            isActive={viewMode === 'list'}
+            onClick={() => SetViewMode('list')}
           />
         </div>
       </div>
