f052bd45624c98d3826c3a84b058bfdc03be0633
smarthulala <chao.long.au@gmail.com>
2023-06-24 22:54:44 +1000

    Refactor: structure refactor

--- src/pages/CreateReport/components/Content/component/Form/Form.tsx
+++ src/pages/CreateReport/components/Content/component/Form/Form.tsx
@@ -1,25 +1,12 @@
 import { FC } from 'react'
 import Button from '@/components/Button'
 import formFields from './assets/formFieldsData'
+import Item from './components/Item'
 
 const Form: FC = () => (
   <div className="bg-white border border-gray px-12 py-10 rounded-2xl shadow">
     {formFields.map(({ title, info }) => (
-      <div key={title}>
-        <div className="border-b border-gray text-2xl font-bold mb-4">{title}</div>
-        <div>
-          {info.map(({ label, type, placeholder }) => (
-            <div key={label} className="mb-4">
-              <div>{label}</div>
-              <input
-                className="h-10 w-full border border-gray rounded px-4 focus:outline-none"
-                type={type}
-                placeholder={placeholder}
-              />
-            </div>
-          ))}
-        </div>
-      </div>
+      <Item key={title} title={title} info={info} />
     ))}
     <div className="text-center mt-10">
       <Button className="w-[200px] bg-primary">Next</Button>
