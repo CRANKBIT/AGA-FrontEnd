8fb4f6da1d9149b48e22f71dee85194ad24c5028
LingHan.S <LINGKUI0324@GMAIL.COM>
2023-06-25 11:30:34 +1000

    we dont need this Dashboard actuly

--- src/pages/MyAccount/MyAccount.tsx
+++ src/pages/MyAccount/MyAccount.tsx
@@ -2,9 +2,15 @@ import { FC } from 'react'
 import { Link } from 'react-router-dom'
 
 const MyAccount: FC = () => (
-  <Link to="/user/dashboard">
-    <div>this is Dashboard</div>
-  </Link>
+  <>
+    <div>
+      this is Dashboard and there should have a company section and click here you will be redirected to the company
+      report page for now
+    </div>
+    <Link to="/user/my-reports">
+      <div className="text-lime-600"> demo company one </div>
+    </Link>
+  </>
 )
 
 export default MyAccount
