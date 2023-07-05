142246871c2df855afc869af20e449e637d17be2
smarthulala <chao.long.au@gmail.com>
2023-07-05 12:30:35 +1000

    refactor: css fix

--- src/pages/ForgotPassword/components/content/Content.tsx
+++ src/pages/ForgotPassword/components/content/Content.tsx
@@ -3,12 +3,16 @@ import Button, { Variant } from '@/components/Button'
 
 const Content: FC = () => (
   <div>
-    <div className="text-2xl mb-8">Forgot Password?</div>
-    <div className="text-gray text-xs">Please enter your email address to reset your password</div>
+    <div className="text-2xl mb-6">Forgot Password?</div>
+    <div className="text-[rgb(165,165,165)]">Please enter your email address to reset your password</div>
     <div>
       <div className="my-14">
-        Email
-        <input type="text" placeholder="example@abc.com.." className="border w-full rounded p-2" />
+        <div className="text-2xl">Email :</div>
+        <input
+          type="text"
+          placeholder="example@abc.com.."
+          className="border w-full rounded p-2 mt-1 focus:outline-none"
+        />
       </div>
       <Button variant={Variant.Primary} className="w-[280px] h-[50px]">
         Reset
