79fe2fc3002f4dfae521c59d7dc860a74f1c371b
Tao <taogit2023@gmail.com>
2023-07-09 16:01:26 +1000

    CB-117

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -16,6 +16,7 @@ import CreateAccount from '@/pages/CreateAccount'
 import MyReportsHistory from '@/pages/MyReportsHistory'
 import CreateReport from '@/pages/CreateReport'
 import ForgotPassword from '@/pages/ForgotPassword'
+import AboutUs from '@/pages/AboutUs'
 
 const isLoggedIn = true
 
@@ -83,6 +84,10 @@ const router = createHashRouter([
   {
     path: '/auth/login/forgot-password',
     element: <ForgotPassword />,
+  },
+  {
+    path: '/about-us',
+    element: <AboutUs />,
   },
 ])
 
