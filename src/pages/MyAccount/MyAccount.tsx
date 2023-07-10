ef6b0d86a275d2b086935f7e73976e48251d0611
Tao <taogit2023@gmail.com>
2023-07-10 23:06:38 +1000

    modify account page

--- src/pages/MyAccount/MyAccount.tsx
+++ src/pages/MyAccount/MyAccount.tsx
@@ -4,15 +4,24 @@ import CompanyLayout from '@/layouts/UserLayout/UserLayout'
 
 const MyAccount: FC = () => (
   <CompanyLayout>
-    <div className="bg-userContent min-h-screen p-10">
-      <h1>Hi, Alonso</h1>
-      <div className="text-end">Edit</div>
-      <div className="flex justify-around">
-        <div> email</div> <div>123.com</div>
+    <div className="bg-userContent min-h-screen p-10 ">
+      <h1>Hi, Tao</h1>
+      <div className="bg-white rounded-lg p-10">
+        <div>
+          <p className="inline-block">Name:</p>
+          <p className="inline-block ml-20">Tao</p>
+        </div>
+        <div>
+          <p className="inline-block">Email:</p>
+          <p className="inline-block ml-20">hello@123.com</p>
+        </div>
+        <div>
+          <p className="inline-block">DOB:</p>
+          <p className="inline-block ml-20">08/09/1999</p>
+        </div>
       </div>
       <div>
-        <div className="text-end">Edit</div>
-        <h1>companylist</h1>
+        <h1>CompanyList</h1>
         <div className="flex gap-10">
           <Link to="/user/my-reports">
             <img src="./svg/CompanyOne.png" alt="Company Logo" className="h-[100px] rounded-[100px]" />
