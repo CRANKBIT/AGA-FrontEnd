ae475fd24a860f1f34a45ea9841b58b668f70418
Tao <taogit2023@gmail.com>
2023-07-11 22:43:07 +1000

    CB-131

---
+++ src/pages/ServiceItem/ServiceItem.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react'
+import CompanyLayout from '@/layouts/CompanyLayout'
+
+const ServiceItem: FC = () => (
+  <CompanyLayout>
+    <div>ServiceItem</div>
+  </CompanyLayout>
+)
+export default ServiceItem
