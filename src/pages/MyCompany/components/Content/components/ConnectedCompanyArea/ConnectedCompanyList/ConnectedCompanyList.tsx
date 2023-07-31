c434a22ada43deebd9e50e274be91fbd625e353b
smarthulala <chao.long.au@gmail.com>
2023-07-31 12:07:26 +1000

    connect tenant-companies to database

--- src/pages/MyCompany/components/Content/components/ConnectedCompanyArea/ConnectedCompanyList/ConnectedCompanyList.tsx
+++ src/pages/MyCompany/components/Content/components/ConnectedCompanyArea/ConnectedCompanyList/ConnectedCompanyList.tsx
@@ -1,17 +1,44 @@
-import { FC } from 'react'
+import { FC, useState, useEffect } from 'react'
+import axios from 'axios'
 import MockData from './CompanyItem/assets/MockData'
 import CompanyItem from './CompanyItem'
 
-const ConnectedCompanyList: FC = () => (
-  <div>
-    <div className="w-[808px] h-[47px] flex items-center border-b border-richBlack border-opacity-20">
-      <span className="text-[14px] text-richBlack font-medium opacity-70 w-[510px] ml-16">Company Name</span>
-      <span className="text-[14px] text-richBlack font-medium opacity-70">Owner</span>
+const ConnectedCompanyList: FC = () => {
+  const [tenantId, getTenantId] = useState('')
+  const [companyData, setCompanyData] = useState([])
+
+  useEffect(() => {
+    const urlTenantId = window.location.search.split('=')[1]
+    getTenantId(urlTenantId || '')
+  }, [])
+
+  useEffect(() => {
+    const fetchData = async (): Promise<void> => {
+      try {
+        const response = await axios.get(`http://localhost:8080/companies/${tenantId}`)
+        const { companies } = response.data
+
+        setCompanyData(companies)
+        console.log(companies)
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
+    }
+
+    fetchData()
+  }, [tenantId])
+
+  return (
+    <div>
+      <div className="w-[808px] h-[47px] flex items-center border-b border-richBlack border-opacity-20">
+        <span className="text-[14px] text-richBlack font-medium opacity-70 w-[510px] ml-16">Company Name</span>
+        <span className="text-[14px] text-richBlack font-medium opacity-70">Owner</span>
+      </div>
+      {MockData.map((item) => (
+        <CompanyItem key={item.id} {...item} />
+      ))}
     </div>
-    {MockData.map((item) => (
-      <CompanyItem key={item.id} {...item} />
-    ))}
-  </div>
-)
+  )
+}
 
 export default ConnectedCompanyList
