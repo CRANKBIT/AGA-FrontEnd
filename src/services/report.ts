88a64e3fca039e3d77d8e4cfdac859b55c9d8c0a
Alan <prossliu@gmail.com>
2023-08-06 17:30:31 +0930

    done company

--- src/services/report.ts
+++ src/services/report.ts
@@ -2,8 +2,18 @@ import { AxiosResponse } from 'axios'
 import api from '@/utils/axios'
 import { Report } from '@/interfaces/report'
 
-export const createReportApi = (report: Report): Promise<AxiosResponse> => api.post('/report/createReport', report)
+export const createReportApi = (report: Report): Promise<AxiosResponse> =>
+  api('/report/createReport', {
+    method: 'POST',
+    data: report,
+  })
 
-export const getReportByIdApi = (id: number): Promise<AxiosResponse> => api.get(`/report/${id}`)
+export const getReportByIdApi = (id: number): Promise<AxiosResponse> =>
+  api(`/report/${id}`, {
+    method: 'GET',
+  })
 
-export const getReports = (): Promise<AxiosResponse> => api.get(`/report`)
+export const getReports = (): Promise<AxiosResponse> =>
+  api('/report', {
+    method: 'GET',
+  })
