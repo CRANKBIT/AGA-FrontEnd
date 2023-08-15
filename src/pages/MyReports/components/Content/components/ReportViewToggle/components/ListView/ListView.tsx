87d050c01bd75b053d543df23c77e69d699638fe
Tao <taogit2023@gmail.com>
2023-08-15 22:15:40 +1000

    connect database report

--- src/pages/MyReports/components/Content/components/ReportViewToggle/components/ListView/ListView.tsx
+++ src/pages/MyReports/components/Content/components/ReportViewToggle/components/ListView/ListView.tsx
@@ -1,19 +1,29 @@
-import { FC, useEffect } from 'react'
+/* eslint-disable no-underscore-dangle */
+import { FC, useEffect, useState } from 'react'
 import ListItem from './components/ListItem'
 import mockData from './assets/mockData'
-import { getReports } from '@/services/report';
+import { getReports } from '@/services/report'
+
+interface ServiecReport {
+  Year: string
+  Make: string
+  Model: string
+  Rego: string
+  Odometer: string
+  CustomerName: string
+  service: string[]
+  _id: string
+}
 
 const ListView: FC = () => {
-  useEffect(()=>{
-    const reportData = getReports();
-    console.log('hello')
-  },[])
-  return (
-  <>
-    {mockData.map((item) => (
-      <ListItem key={item.id} {...item} />
-    ))}
-  </>
-)} 
+  const [reportData, setReportData] = useState<ServiecReport[]>([])
+  useEffect(() => {
+    getReports().then((response) => {
+      setReportData(response.data)
+    })
+  }, [])
+  return (<>
+  {reportData.map((item) => console.log(item._id))}</>)
+}
 
 export default ListView
--- src/pages/MyReports/components/Content/components/ReportViewToggle/components/ListView/components/ListItem/ListItem.tsx
+++ src/pages/MyReports/components/Content/components/ReportViewToggle/components/ListView/components/ListItem/ListItem.tsx
@@ -3,27 +3,25 @@ import { AiOutlineArrowRight } from 'react-icons/ai'
 import { FaCircle } from 'react-icons/fa'
 
 interface Props {
-  report: string
-  lastUpdated: string
-  task: number
+  Rego: string
+  Make: string
+  CustomerName: string
 }
 
-const ListItem: FC<Props> = ({ report, lastUpdated, task }) => (
+const ListItem: FC<Props> = ({ Rego, Make, CustomerName }) => (
   <div className="flex items-center gap-20 h-[84px] mb-[20px] border-b-[1px] border-blue-900 border-opacity-20">
     <div className="font-bold min-w-[140px]">
-      {report}
+      {Rego}
       <div className="font-normal text-gray-600">
         <FaCircle className="text-green-400 inline-block mr-3" />
         Active
       </div>
     </div>
     <div className="min-w-[140px]">
-      {lastUpdated}
-      <div>You</div>
+      {Make}
     </div>
     <div className=" min-w-[140px]">
-      <div className="font-bold">{task}</div>
-      <div>8 today</div>
+      <div className="font-bold">{CustomerName}</div>
     </div>
     <div className="cursor-pointer text-[#007AD3] flex items-center">
       <div className="underline">See More</div>
